9280ba34b40a1c60121d3270566192487250ddce
Luciagase <113980773+Luciagase@users.noreply.github.com>
2022-09-29 11:30:01 +0000

    main

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('hi')
+#variable del tiempo de cocción y tiempo de preparación
+EXPECTED_BAKE_TIME=40
+PREPARATION_TIME=2
+
+#función que coja el tiempo en minutos que lleva la lasaña en el horno y devuelva el tiempo que le queda de cocción
+def bake_time_remaining(elapsed_bake_time):
+  return EXPECTED_BAKE_TIME - elapsed_bake_time
+
+#función que calcule el tiempo de preparación cogiendo el numero de capas de la lasaña y multiplicando el tiempo que se tarda en preparar cada capa
+def preparation_time_in_minutes(number_of_layers):
+  return number_of_layers * PREPARATION_TIME
+
+
+#función que calcule el tiempo que llevas cocinando la lasaña mediante la suma de la preparación y el tiempo que ya lleva en el horno
+def elapsed_time_in_minutes(number_of_layers,elapsed_bake_time):
+  return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
